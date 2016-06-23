53d632a4faed937c3f890323ac170e408e9eeda8
zhanglongtai <neo_seele@hotmail.com>
2016-06-23 17:32:26 +0800

    update lang

---
+++ src/lang/cn.js
@@ -0,0 +1,9 @@
+export const cnPackage = {
+	TitleUploadFiles: "上传文件",
+    TitleSpecifyFileName: "指定文件名",
+    BtnUploadFile: "上传文件",
+    BtnSubmit: "提交",
+    TitleModelViewport: "模型显示",
+    BtnMesh: "网格",
+    BtnReset: "重置视角",
+};
---
+++ src/lang/en.js
@@ -0,0 +1,9 @@
+export const enPackage = {
+	TitleUploadFiles: "Upload Files",
+    TitleSpecifyFileName: "Specify File Name",
+    BtnUploadFile: "Upload file",
+    BtnSubmit: "Submit",
+    TitleModelViewport: "Model Viewport",
+    BtnMesh: "Mesh",
+    BtnReset: "Reset",
+};
